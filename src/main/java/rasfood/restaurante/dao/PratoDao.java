c4aa206694b172fa4115e25e17d499f7e03e8011
Wenderson Santos <guto15santos@gmail.com>
2022-02-07 09:59:36 -0300

    Testando os métodos DAO

--- src/main/java/rasfood/restaurante/dao/PratoDao.java
+++ src/main/java/rasfood/restaurante/dao/PratoDao.java
@@ -17,11 +17,7 @@ public class PratoDao {
 
 	public void cadastrar(final Prato prato) {
 		em.persist(prato);
-		System.out.println("Nome do prato: " + prato.getNome());
-		System.out.println("Descrição do prato: " + prato.getDescricao());
-		System.out.println("Registro do prato: " + prato.getDataRegistro());
-		System.out.println("Valor do prato: " + prato.getValor());
-		System.out.println("Disponivel do prato: " + prato.isDisponivel());
+		
 	}
 
 	public Prato consultar(final Long id) {
--- src/main/java/rasfood/restaurante/service/teste/PratoService.java
+++ src/main/java/rasfood/restaurante/service/teste/PratoService.java
@@ -18,22 +18,38 @@ public class PratoService {
 		
 		EntityManager em = JpaUtil.getEmf();
 		em.getTransaction().begin();
-		
+		PratoDao pratoDao = new PratoDao(em);
+		//adicionando
 		Prato risoto = new Prato();
 		risoto.setNome("Risoto de frutos do mar");
 		risoto.setDescricao("Risoto acompanhado de lula, polvo e mariscos");
 		risoto.setDisponivel(true);
 		risoto.setDataRegistro(LocalDateTime.now());
 		risoto.setValor(BigDecimal.valueOf(88.50));
-
-		PratoDao pratoDao = new PratoDao(em);
 		pratoDao.cadastrar(risoto);
-
+		//adicionando
+		Prato salmao = new Prato();
+		salmao.setNome("Salmao grelhado ao molho de maracujá");
+		salmao.setDescricao("Salmao grelhado ao molho de maracuja");
+		salmao.setDisponivel(true);
+		salmao.setDataRegistro(LocalDateTime.now());
+		salmao.setValor(BigDecimal.valueOf(60.99));
+		pratoDao.cadastrar(salmao);
+		//consultado
+		lerInformacoes(em.find(Prato.class, 2L));
 		
-		em.persist(risoto);
+		
 		em.getTransaction().commit();
 		em.close();
 		JpaUtil.close();
+	}
+	
+	public static void lerInformacoes(Prato prato) {
+		System.out.println("Nome do prato: " + prato.getNome());
+		System.out.println("Descrição do prato: " + prato.getDescricao());
+		System.out.println("Registro do prato: " + prato.getDataRegistro());
+		System.out.println("Valor do prato: " + prato.getValor());
+		System.out.println("Disponivel do prato: " + prato.isDisponivel());
 	}
 
 }
