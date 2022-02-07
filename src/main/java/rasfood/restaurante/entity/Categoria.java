b7dd0a6c3bbe79594fb101a1efecb3346622e739
Wenderson Santos <guto15santos@gmail.com>
2022-02-07 16:26:08 -0300

    Relacionamento ManyToOne

--- src/main/java/rasfood/restaurante/entity/Categoria.java
+++ src/main/java/rasfood/restaurante/entity/Categoria.java
@@ -1,5 +1,43 @@
 package rasfood.restaurante.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
 public class Categoria {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
